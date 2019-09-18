3b49929db447d254f0f033fb13f99f349d8e709a
User <ilya.j0b@yandex.ru>
2019-09-18 17:51:40 +0300

    init foundVideo component

---
+++ src/features/find-videos/components/FoundVideo.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "../../../assets/styles/_FoundVideo.module.scss";
+
+interface Props {
+  img: string;
+  title: string;
+  author: string;
+  description: string;
+}
+
+export const FoundVideo: React.FC<Props> = ({
+  img,
+  title,
+  author,
+  description
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.styles}>
+        <div className={styles.img}>
+          <img src={img} />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h3 className={styles.headline}>{title}</h3>
+            <div className={styles.info}>{author}</div>
+            <div className={styles.info}>45 тыс. просмотров</div>
+            <div className={styles.info}> Неделю назад</div>
+          </div>
+          <div className={styles.descr}>{`${description.slice(0, 55)}...`}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
