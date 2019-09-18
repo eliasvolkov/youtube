d89d82de1e3a73e2f084e0cdda89f55f9365ecd0
User <ilya.j0b@yandex.ru>
2019-09-18 12:12:25 +0300

    init HotVideoComponent

--- src/features/hot-videos/components/HotVideo.tsx
+++ src/features/hot-videos/components/HotVideo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../../../assets/styles/_HotVideo.module.scss";
 
 interface Props {
   img: string;
@@ -9,14 +10,14 @@ interface Props {
 
 export const HotVideo: React.FC<Props> = ({ img, title, author, views }) => {
   return (
-    <div className="card">
-      <div className="img">
+    <div className={styles.card}>
+      <div className={styles.img}>
         <img src={img} alt="video-img" />
       </div>
-      <h3 className="headline">{title}</h3>
-      <div className="author">{author}</div>
-      <div className="views">просмотров {views} тыс.</div>
-      <div className="time"> Неделю назад</div>
+      <h3 className={styles.headline}>{title}</h3>
+      <div className={styles.author}>{author}</div>
+      <div className={styles.views}>просмотров {views} тыс.</div>
+      <div className={styles.time}> Неделю назад</div>
     </div>
   );
 };
