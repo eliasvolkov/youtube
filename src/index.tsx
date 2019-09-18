0ca9c6c9c1efa03868d30c6a90fd06205fbe6dc1
User <ilya.j0b@yandex.ru>
2019-09-18 12:11:40 +0300

    init redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+
+import { Provider } from "react-redux";
+import { reducer as topVideos } from "./features/hot-videos/reducer";
+import createSagaMiddleware from "redux-saga";
+import { fork } from "redux-saga/effects";
+import { getVideosStart } from "./features/hot-videos/saga";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+const rootReducer = combineReducers({
+  topVideos
+});
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+function* rootSaga() {
+  yield fork(getVideosStart);
+}
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
