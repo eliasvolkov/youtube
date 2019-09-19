095cf2f4c67f41a4dc92b2d6d654752d26587959
User <ilya.j0b@yandex.ru>
2019-09-19 11:26:09 +0300

    init routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Index } from "./routes/Index";
+import "./assets/styles/style.scss";
+import { FoundVideosPage } from "./routes/FoundVideosPage";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/videos" exact component={FoundVideosPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
