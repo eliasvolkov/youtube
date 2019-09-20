e40e227d304ba3e226ab73e6e9823c536c6106a6
User <ilya.j0b@yandex.ru>
2019-09-20 14:04:18 +0300

    add spinner

--- src/features/find-videos/components/FoundVideoContainer.tsx
+++ src/features/find-videos/components/FoundVideoContainer.tsx
@@ -1,29 +1,36 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { FoundVideo } from "./FoundVideo";
+import { filterVideos } from "../selector";
 import styles from "../../../assets/styles/_FoundVideoContainer.module.scss";
+import { Spinner } from "../../../components/Spinner";
 
 export interface IFoundVideoContainerProps {
   foundVideos: any;
+  loading: boolean;
 }
 
 class FoundVideoContainer extends React.Component<IFoundVideoContainerProps> {
   public render() {
-    const { foundVideos } = this.props;
+    const { foundVideos, loading } = this.props;
     return (
       <div className={styles.container}>
         <div className={styles.row}>
-          {foundVideos.map((video: any) => {
-            return (
-              <FoundVideo
-                key={video.id.videoId}
-                img={video.snippet.thumbnails.medium.url}
-                title={video.snippet.title}
-                author={video.snippet.channelTitle}
-                description={video.snippet.description}
-              />
-            );
-          })}
+          {loading ? (
+            <Spinner />
+          ) : (
+            foundVideos.map((video: any) => {
+              return (
+                <FoundVideo
+                  key={video.id.videoId}
+                  img={video.snippet.thumbnails.medium.url}
+                  title={video.snippet.title}
+                  author={video.snippet.channelTitle}
+                  description={video.snippet.description}
+                />
+              );
+            })
+          )}
         </div>
       </div>
     );
@@ -32,7 +39,8 @@ class FoundVideoContainer extends React.Component<IFoundVideoContainerProps> {
 
 const mapState2Props = (state: any) => {
   return {
-    foundVideos: state.foundVideos.data
+    foundVideos: filterVideos(state.foundVideos.data),
+    loading: state.foundVideos.loading
   };
 };
 
